61557b9f49968b3e9e320822b5f0c2dbcc1caa90
Ezekiel Ilori <ilorieazykiel@gmail.com>
2019-11-22 14:23:45 +0100

    React lazy and suspense

--- src/components/Sections/Sections.tsx
+++ src/components/Sections/Sections.tsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import Feature from './Feature/Feature';
+import React, { lazy, Suspense } from 'react';
 import FeaturedIn from './FeaturedIn/FeaturedIn';
 import Story from './Story/Story';
 import NewsLetter from './NewsLetter/NewsLetter';
 
+const Feature = lazy(() =>import('./Feature/Feature'));
 import {
   FeatureProps, StoriesProps, BecomeAHost, OurStory, GearProps, FeaturedInProps,
 } from './data';
 
 const Sections: React.FC = () => (
   <>
-    <Feature  cards={FeatureProps.cards} title={FeatureProps.title} />
-    <Story {...BecomeAHost} />
-    <Feature {...StoriesProps} />
-    <Story {...OurStory} />
-    <FeaturedIn {...FeaturedInProps} />
-    <Feature {...GearProps} />
+    <Suspense fallback={<div>Loading...</div>}>
+      <Feature  cards={FeatureProps.cards} title={FeatureProps.title} />
+      <Story {...BecomeAHost} />
+      <Feature {...StoriesProps} />
+      <Story {...OurStory} />
+      <FeaturedIn {...FeaturedInProps} />
+      <Feature {...GearProps} />
+    </Suspense>
     <NewsLetter />
   </>
 );
